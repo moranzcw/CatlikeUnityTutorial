13155038e241f680cc7678d4f239e365403001b9
moranzcw <moranzcw@gmail.com>
2020-08-12 02:49:33 +0800

    feat: add "Movement - 1.Sliding a Sphere" project

---
+++ Movement/1.Sliding a Sphere/Assets/Scripts/MovingSphere.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f; // 最大速度
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f; // 最大加速度
+    [SerializeField, Range(0f, 1f)]
+    float bounciness = 0.5f;  // 弹性恢复系数
+    [SerializeField]
+    Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+
+    Vector3 velocity;
+
+    void Update()
+    {
+        // 获取输入
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+        // 使用期望速度来更新速度
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        // 计算位移
+        Vector3 displacement = velocity * Time.deltaTime;
+        Vector3 newPosition = transform.localPosition + displacement;
+
+        // 检测边缘碰撞
+        if (newPosition.x < allowedArea.xMin)
+        {
+            newPosition.x = allowedArea.xMin;
+            velocity.x = -velocity.x * bounciness; // 反弹
+        }
+        else if (newPosition.x > allowedArea.xMax)
+        {
+            newPosition.x = allowedArea.xMax;
+            velocity.x = -velocity.x * bounciness;
+        }
+        if (newPosition.z < allowedArea.yMin)
+        {
+            newPosition.z = allowedArea.yMin;
+            velocity.z = -velocity.z * bounciness;
+        }
+        else if (newPosition.z > allowedArea.yMax)
+        {
+            newPosition.z = allowedArea.yMax;
+            velocity.z = -velocity.z * bounciness;
+        }
+        transform.localPosition = newPosition;
+    }
+}
